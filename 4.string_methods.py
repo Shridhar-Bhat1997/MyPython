91b0bfed4617226497c4ae8378da22aef3572f39
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-09-21 22:29:21 +0530

    Python String methods 21-Sep-2022

---
+++ 4.string_methods.py
@@ -0,0 +1,13 @@
+# Python string methods
+
+name="dravid"
+print(len(name)) # returns length of the string.
+print(name.find("d")) # finds the first occurrence of the specified value.
+print(name.capitalize()) # converts the first character of a string to an uppercase letter.
+print(name.upper()) # converts given string to upper case.
+print(name.lower()) # converts given string to lower case.
+print(name.isdigit()) # returns True if all the characters are digits, otherwise False.
+print(name.isalpha()) # returns True if all the characters are alphabet letters.
+print(name.count("d")) # returns the number of times a specified value appears in the string.
+print(name.replace("a","s")) # replaces a specified phrase with another specified phrase.
+print(name*3)
