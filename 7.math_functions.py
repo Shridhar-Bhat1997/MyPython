48fe16c0ba5c5272a2988810c542a953fb21f763
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-09-21 23:34:54 +0530

    Python math functions 21-Sep-2022

---
+++ 7.math_functions.py
@@ -0,0 +1,15 @@
+import math
+pi=3.14
+
+print(round(pi)) # returns a floating-point number rounded to the specified number of decimals.
+print(math.ceil(pi)) # rounds a number UP to the nearest integer.
+print(math.floor(pi)) # rounds a number DOWN to the nearest integer.
+print(abs(pi)) # returns the absolute value of the given number. ie -10 is also 10.
+print(pow(3,2)) # returns the value of x to the power of y.
+print(math.sqrt(49)) # returns square root of any number.
+
+a=10
+b=23
+c=56
+print(max(a,b,c)) # returns highest value
+print(min(a,b,c)) # returns lowest value
