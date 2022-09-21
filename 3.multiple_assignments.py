263a6090ab2517118871d15692fa31360844229c
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-09-21 21:31:22 +0530

    Python multiple-assignments 21-Sep-2022

---
+++ 3.multiple_assignments.py
@@ -0,0 +1,23 @@
+# multiple assignments in python
+
+name="john"
+age=23
+attractive=True
+
+print(name)
+print(age)
+print(attractive)
+
+firstname,lastname,location,age="Saurav","Ganguly","Kolkatta",56
+print(firstname)
+print(lastname)
+print(location)
+print(age)
+
+Sandy=John=Ravi=30
+print(Sandy)
+print(John)
+print(Ravi)
+
+
+
