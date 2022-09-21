53fe493e1637afa4894e469b54f1bb28e9334500
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-09-21 23:00:19 +0530

    Python user input 21-Sep-2022

---
+++ 6.user_input.py
@@ -0,0 +1,8 @@
+name=input("what is your name:")
+print("Hello " + name) 
+
+age=int(input("How old are you?:"))
+print("You are " + str(age)+ " years old")
+
+height=float(input("how tall are you?: "))
+print("you are " + str(height) + "cm tall")
