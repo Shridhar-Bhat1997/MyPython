c24f252194ac16474f9febb5dc35c940866b5093
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-09-22 16:57:06 +0530

    Python tuples 22-Sep-2022

---
+++ 17.tuples.py
@@ -0,0 +1,15 @@
+# tuples --> collection which is ordered and unchangeable..(immutable)
+
+student=("john",25,"male")
+
+print(student.count("male"))
+
+print(student.index("john"))
+
+#iterate over the tuple
+
+for x in student:
+    print(x)
+    
+if "john" in student:
+    print("john is here!")
