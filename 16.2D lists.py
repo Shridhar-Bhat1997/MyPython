265b66084df593b65335b61a58f52a88f9124618
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-09-22 16:46:20 +0530

    Python 2D lists 22-Sep-2022

---
+++ 16.2D lists.py
@@ -0,0 +1,14 @@
+# 2D lists= a list of lists
+
+classes=["std1","std2","std3"]
+students=[59,34,90]
+play_games=["cricket","carrom","hockey"]
+
+school=[classes,students,play_games]
+print(school)
+
+print(school[0][2])
+
+print(school[1][1])
+
+print(school[2][0])
