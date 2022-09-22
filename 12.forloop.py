6df600c74d81c2739a71afb8cb64d188551a2898
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-09-22 11:46:28 +0530

    Python for loop 22-Sep-2022

---
+++ 12.forloop.py
@@ -0,0 +1,19 @@
+# python for loop
+
+import time
+
+for i in range(50,100,2):
+    print(i)
+    
+for i in "Saurav Ganguly":
+    print(i)
+    
+    
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy journey")
+
+    
+
+    
