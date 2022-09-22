f0cfa92752270d4e2bf18ffdcd5e1adf4539c01c
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-09-22 19:06:22 +0530

    Python functions 22-Sep-2022

---
+++ 21.functions.py
@@ -0,0 +1,21 @@
+# functions --> a block of code which is executed only when it is called...
+
+def repeat():
+    print("I will not repeat my code")
+    print("Have a nice day")
+    
+repeat()
+
+
+def hello(name): # function with parameter
+    print("Hello " + name)
+    print("good morning")
+    
+hello("Tom")
+
+
+def greeting(first_name,last_name,age):
+    print("Hello "+ first_name + " " + last_name)
+    print("You are "+ str(age) + " years old")
+    
+greeting("Cartoon","Network",44)
