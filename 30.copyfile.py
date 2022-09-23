fd7c188b1954c70de026361e41e0669d9a599c2b
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-09-23 19:28:04 +0530

    Python copy a file 23-Sep-2022

---
+++ 30.copyfile.py
@@ -0,0 +1,8 @@
+# copyfile() --> copies contents of a file.
+# copy() --> copyfile() + permission mode + destination can be directory.
+# copy2() --> copy() + copies metadata (file's creation and modification time)
+
+import shutil
+
+shutil.copyfile("test.txt","copy.txt")
+
