25b7bd92bdb10d5a75235002ba34535060e5e36e
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-10-10 11:08:41 +0530

    Python filter function 10-Oct-2022

---
+++ filter.py
@@ -0,0 +1,18 @@
+# filter() --> creates a collection of elements from an iterable for which a function
+#              returns true.
+
+# filter(function,iterable)
+
+friends=[("jitu",23),
+         ("monica",55),
+         ("nitya",43),
+         ("joel",56),
+         ("joy",12),
+         ("suhan",10)]
+
+age=lambda data:data[1] >=18
+
+drinking_buddies=list(filter(age,friends))
+
+for i in drinking_buddies:
+    print(i)
